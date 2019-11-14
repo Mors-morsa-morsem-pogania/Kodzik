0f26e8ece6ccea087d2dabc588c54e1c105b5fdd
NiecnyKarzel <57544026+NiecnyKarzel@users.noreply.github.com>
2019-11-14 20:56:11 +0100

    Update load_and_dict.py

--- load_and_dict.py
+++ load_and_dict.py
@@ -1,66 +1,49 @@
 import wave
-from scipy.io import wavfile as wav
 import numpy as np
 from os import listdir
 import scipy
 import matplotlib.pyplot as plt
 import sklearn
 import sklearn.mixture
+import sklearn.model_selection
+from scipy.io import wavfile as wav
+from os import listdir
 import pickle
 from collections import defaultdict
+import python_speech_features as psf
 
-# odczyt z pliku listy mfcc_nazwa
 nazwa_pliku = 'lista_mfcc_nazwa.pkl'
 pkl_file = open(nazwa_pliku, 'rb')
 
 lista_mfcc_nazwa = pickle.load(pkl_file)
 pkl_file.close()
 
-# tworzenie słownika
-
 slownik = defaultdict(list)  # stworzenie pustego słownika
-
-lista_plikow=[] #stworzenie pustej listy i wydobycie nazw ze środka
+lista_plikow=[]
 for i in range(0,len(lista_mfcc_nazwa)-1):
-    lista_plikow.append(lista_mfcc_nazwa[i][1]) 
+    lista_plikow.append(lista_mfcc_nazwa[i][1]) # nie wiem jak to wydobyć ze środka :/
 
+# print(lista_plikow)
 for i in range(0, 21):
     if i == 0:
         slownik[str(lista_plikow[i * 10 + 1])[0:3]] = lista_mfcc_nazwa[0:10]  # wybranie 10 cyfr dla danego mówcy i przypisanie ich do klucza (mówcy w słowniku)
     else:
         slownik[str(lista_plikow[i * 10 + 1])[0:3]] = lista_mfcc_nazwa[(10 * i):(10 * i + 10)]
 
-
 print(len(slownik))  # dlugość 22 - zgadza sie
 print(slownik["AO1"][1][1])
 
-# Przydatne:
-# # Odwołanie sie do wiersza w słowniku: np. slownik["AO1"]
-# print(slownik["AO1"])
-# # Odwołanie sie do wiersza w liście która jest w słowniku: np. slownik["AO1"][0]
-# print(slownik["AO1"][0])
-# # Odwołanie sie do macierzy MFCC w liście w słowniku: np. slownik["AO1"][0][0]
-# print(slownik["AO1"][0][0])
-# # Odwołanie sie do nazwy całego pliku w liście w słowniku: np. slownik["AO1"][0][1]
-# print(slownik["AO1"][0][1])
-
 def wszystkie_cyfry(nr_cyfry,lista_mfcc_nazwa):
     cyfry=[]
-    for i in range(0, 22):
+
+    for i in range(0, int(len(lista_mfcc_nazwa)/10)):
         cyfry.extend(lista_mfcc_nazwa[i * 10+nr_cyfry][0])
     cyfry = np.array(cyfry)
     return cyfry
 
-wszystkie_cyfry_0=wszystkie_cyfry(0,lista_mfcc_nazwa)
-wszystkie_cyfry_1=wszystkie_cyfry(1,lista_mfcc_nazwa)
-wszystkie_cyfry_2=wszystkie_cyfry(2,lista_mfcc_nazwa)
-wszystkie_cyfry_3=wszystkie_cyfry(3,lista_mfcc_nazwa)
-wszystkie_cyfry_4=wszystkie_cyfry(4,lista_mfcc_nazwa)
-wszystkie_cyfry_5=wszystkie_cyfry(5,lista_mfcc_nazwa)
-wszystkie_cyfry_6=wszystkie_cyfry(6,lista_mfcc_nazwa)
-wszystkie_cyfry_7=wszystkie_cyfry(7,lista_mfcc_nazwa)
-wszystkie_cyfry_8=wszystkie_cyfry(8,lista_mfcc_nazwa)
-wszystkie_cyfry_9=wszystkie_cyfry(9,lista_mfcc_nazwa)
+lista_cyfr=[]
+for cyfra in range(0,10):
+    lista_cyfr.append(wszystkie_cyfry(cyfra,lista_mfcc_nazwa))
 
 def GaussianMix(macierz,n,n_iter):
     gmm=sklearn.mixture.GaussianMixture(n_components=n, covariance_type='diag', tol=0.001, reg_covar=1e-06,
@@ -70,25 +53,16 @@ def GaussianMix(macierz,n,n_iter):
     gmm.fit(macierz)
     return gmm
 
-GMM_0=GaussianMix(wszystkie_cyfry_0,8,100)
-GMM_1=GaussianMix(wszystkie_cyfry_1,8,100)
-GMM_2=GaussianMix(wszystkie_cyfry_2,8,100)
-GMM_3=GaussianMix(wszystkie_cyfry_3,8,100)
-GMM_4=GaussianMix(wszystkie_cyfry_4,8,100)
-GMM_5=GaussianMix(wszystkie_cyfry_5,8,100)
-GMM_6=GaussianMix(wszystkie_cyfry_6,8,100)
-GMM_7=GaussianMix(wszystkie_cyfry_7,8,100)
-GMM_8=GaussianMix(wszystkie_cyfry_8,8,100)
-GMM_9=GaussianMix(wszystkie_cyfry_9,8,100)
-
-wszystkie_GMM=[GMM_0,GMM_1,GMM_2,GMM_3,GMM_4,GMM_5,GMM_6,GMM_7,GMM_8,GMM_9]
+wszystkie_GMM=[]
+for gaussian in range(0,10):
+    wszystkie_GMM.append(GaussianMix(lista_cyfr[gaussian],8,100))
 
 def prawdopodobienstwo_cyfry(gmmx,mfcc_cyfry):
-    p = np.exp(gmmx.score(mfcc_cyfry)) * 0.1 / (np.exp(GMM_0.score(mfcc_cyfry)) * 0.1 + np.exp(GMM_1.score(mfcc_cyfry)) * 0.1
-                                             + np.exp(GMM_2.score(mfcc_cyfry)) * 0.1 + np.exp(GMM_3.score(mfcc_cyfry)) * 0.1
-                                             + np.exp(GMM_4.score(mfcc_cyfry)) * 0.1 + np.exp(GMM_5.score(mfcc_cyfry)) * 0.1
-                                             + np.exp(GMM_6.score(mfcc_cyfry)) * 0.1 + np.exp(GMM_7.score(mfcc_cyfry)) * 0.1
-                                             + np.exp(GMM_8.score(mfcc_cyfry)) * 0.1 + np.exp(GMM_9.score(mfcc_cyfry)) * 0.1)
+    p = np.exp(gmmx.score(mfcc_cyfry)) * 0.1 / (np.exp(wszystkie_GMM[0].score(mfcc_cyfry)) * 0.1 + np.exp(wszystkie_GMM[1].score(mfcc_cyfry)) * 0.1
+                                             + np.exp(wszystkie_GMM[2].score(mfcc_cyfry)) * 0.1 + np.exp(wszystkie_GMM[3].score(mfcc_cyfry)) * 0.1
+                                             + np.exp(wszystkie_GMM[4].score(mfcc_cyfry)) * 0.1 + np.exp(wszystkie_GMM[5].score(mfcc_cyfry)) * 0.1
+                                             + np.exp(wszystkie_GMM[6].score(mfcc_cyfry)) * 0.1 + np.exp(wszystkie_GMM[7].score(mfcc_cyfry)) * 0.1
+                                             + np.exp(wszystkie_GMM[8].score(mfcc_cyfry)) * 0.1 + np.exp(wszystkie_GMM[9].score(mfcc_cyfry)) * 0.1)
     return p
 
 p=[]
@@ -98,6 +72,8 @@ for i in range(0,len(wszystkie_GMM)):
 
 print(sum(p)) #sumują sie do 1 - sztos
 
+
+
 xvalid=sklearn.model_selection.KFold(n_splits=5)
 xvalid.get_n_splits(slownik)
 # klucze=list(slownik.keys())
@@ -106,15 +82,36 @@ print(klucze)
 print(xvalid)
 
 for train_ids, test_ids in xvalid.split(slownik):
-    X_train = {}
-    X_test = {}
+    X_train = {} #pusty słownik na dane treningowe
+    X_test = {} #pusty słownik na dane testowe
+    lista_mfcc_nazwa_train=[] #pusta lista na dane treningowe
+    lista_mfcc_nazwa_test=[] #pusta lista na dane testowe
     print("Train: ", train_ids, "Test: ", test_ids)
     for train in train_ids:
-        X_train[klucze[train]]=slownik[klucze[train]]
+        X_train[klucze[train]]=slownik[klucze[train]] #slownik z danymi treningowymi
+        for i in range(0,10):
+            lista_mfcc_nazwa_train.append([slownik[klucze[train]][i][0],slownik[klucze[train]][i][1]]) #lista danych treningowych mfcc + nazwa pliku
     for test in test_ids:
-        X_test[klucze[test]] = slownik[klucze[test]]
-    print(len(X_train),len(X_test))
+        X_test[klucze[test]] = slownik[klucze[test]] #slownik z danymi testowymi
+        for i in range(0, 10):
+            lista_mfcc_nazwa_test.append([slownik[klucze[test]][i][0], slownik[klucze[test]][i][1]])  #lista danych testowych mfcc + nazwa pliku
+
+    lista_cyfr_train=[]
+    for c in range(0,10):
+        lista_cyfr_train.append(wszystkie_cyfry(c,lista_mfcc_nazwa_train)) #duże macierze mfcc dla każdej z cyfr z danych treninowych
+
+    lista_cyfr_test = []
+    for c in range(0, 10):
+        lista_cyfr_test.append(wszystkie_cyfry(c, lista_mfcc_nazwa_test)) #duże macierze mfcc dla każdej z cyfr z danych testowych
+
+    lista_gaussianow_train=[] #lista modeli dla cyfr od 0 do 9
+    for gaussian in range(0,10):
+        lista_gaussianow_train.append(GaussianMix(lista_cyfr_train[gaussian],8,100))
+    a=1
     #przejść po wszstkich trainach i testach na raz
     #nowy model będzie stworzony tylko dla mfcc dla mówców z pociągu
     #stworzenie nowej turbo macierzy mfcc dla pociągu i oddzielnie dla testowych
     #dwa podsłowniki
+
+
+a=1
