b556733da1199e9f850c3c8111a9a890f11e2ded
Ania <57544026+NiecnyKarzel@users.noreply.github.com>
2019-11-20 20:20:24 +0100

    Update wczytywnie_plikow.py

--- wczytywnie_plikow.py
+++ wczytywnie_plikow.py
@@ -17,11 +17,11 @@ for i in range(0, len(lista_plikow)):
     nazwa = lista_plikow[i]  # wydobycie nazwy pliku
     fs, dane_z_pliku = wav.read(("C:\\!STUDIA\\Technologia mowy\\PROJEKT 1 - Klasyfikacja cyfr\\train\\" + nazwa))
     macierz_MFCC=psf.base.mfcc(dane_z_pliku, samplerate=fs,  winlen=0.025, winstep=0.01, numcep=13,
-                                    nfilt=10, nfft=int(0.06*fs), lowfreq=0, highfreq=None, preemph=0.1,
-                                    ceplifter=0, appendEnergy=True, winfunc=np.hamming)
+                                    nfilt=10, nfft=int(0.06*fs), lowfreq=0, highfreq=None, preemph=0.97,
+                                    ceplifter=22, appendEnergy=True, winfunc=np.hamming)
     macierz_MFCC_delta = psf.base.delta(macierz_MFCC, 2)
-    macierz_MFCC_delta_delta = psf.base.delta(macierz_MFCC_delta, 2)
-    macierz_MFCC = np.column_stack((macierz_MFCC, macierz_MFCC_delta, macierz_MFCC_delta_delta))
+    #macierz_MFCC_delta_delta = psf.base.delta(macierz_MFCC_delta, 2)
+    macierz_MFCC = np.column_stack((macierz_MFCC, macierz_MFCC_delta))
     lista_mfcc_nazwa.append([macierz_MFCC, lista_plikow[i]])  # dodanie mfcc i nazwy do listy
 
 
